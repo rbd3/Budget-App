c7f51d00a3ed51cfe7c7465d8269e3e446815f20
rbd3 <rabedesanaandry01@gmail.com>
2023-11-24 20:13:37 +0300

    Add model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
+# spec/models/user_spec.rb
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'John', email: 'john@example.com', password: 'password123', confirmed_at: Time.now) }
+
+  it 'is valid with valid attributes' do
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    user.name = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without a role' do
+    user.role = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid with an invalid role' do
+    user.role = 'invalid_role'
+    expect(user).to_not be_valid
+  end
+
+  it 'is valid with a valid role' do
+    user.role = 'user'
+    expect(user).to be_valid
+  end
+
+  it 'has many groups' do
+    expect(user.groups).to be_empty
+  end
+
+  it 'has many operations' do
+    expect(user.operations).to be_empty
+  end
 end
