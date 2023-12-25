b67aa226166d15988b9a2f6547fe269bcad6b629
Andry Narson <119520276+rbd3@users.noreply.github.com>
2023-12-25 19:05:13 +0300

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,6 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
     user ||= User.new
     can :read, User
     can :read, Group
